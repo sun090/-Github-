8f622d32a96f2ca2842ab40246f40fd60afb1538
test <>
2019-02-22 22:43:02 +0900

    課題４、再提出

---
+++ Kadai4/1.既存課題のブラッシュアップ/page5.js
@@ -0,0 +1,16 @@
+alert('Welcome CA Kadai4');
+
+$(document).ready(function() {
+    $('#xx').on('click',function(){ 
+        $('.about_text').css('color','red');
+        $('.about_sub').css('color','red');
+    });  }); 
+    
+$(document).ready(function() {
+        $('#yy').on('click',function(){ 
+            $('.news_1').css('color','red');
+            
+        });  });     
+
+
+  
